f29333037f7e0b36200243ec9b327bdab08015d2
tuan perera <tuanperera@gmail.com>
2021-11-17 12:07:36 +0100

    show best bid and ask price

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use tungstenite::{connect, Message, WebSocket};
 use tungstenite::client::AutoStream;
 use url::Url;
 
-use crate::models::{LimitOrderBook, OrderData};
+use crate::models::{LimitOrderBook, OrderData, LimitOrder};
 mod models;
 
 static DERIBIT_WS_API: &str = "wss://www.deribit.com/ws/api/v2";
@@ -42,20 +42,89 @@ fn push_data_to_order_book(limit_order_book: &mut LimitOrderBook, data: &serde_j
 	}
 }
 
-fn main() {
-    let deribit_url = format!(
-        "{}/public/subscribe",
-        DERIBIT_WS_API
-    );
-    let (mut socket, response) = connect(Url::parse(&deribit_url).unwrap()).expect("Can't connect.");
+fn populate_limit_order(order_state_raw: &mut serde_json::Value) -> LimitOrder {
+	let data = order_state_raw.get("params").expect("Could not get params from JSON object.")
+								.get("data").expect("Could not get data from JSON object.");
+	
+	let mut limit_order = LimitOrder {
+		order_type: data.get("type").expect("Could not get order type").as_str().unwrap().to_string(),
+    	timestamp: data.get("timestamp").expect("Could not get timestamp").as_u64().unwrap(),
+    	prev_change_id: data.get("prev_change_id").expect("Could not get previous change id").as_u64().unwrap(),
+    	change_id: data.get("change_id").expect("Could not get change id").as_u64().unwrap(),
+    	bids: Vec::new(),
+    	asks: Vec::new(),
+	};
+	// println!("data = {:#?}", data);
+	let asks = data.get("asks").expect("Error extracting asks").as_array().expect("Error extracting array from Value");
+	// println!("Asks = {:#?}", asks);
+	let bids = data.get("bids").expect("Error extracting bids").as_array().expect("Error extracting array from Value");
+	// println!("bids = {:#?}", bids);
+	push_to_limit_order(asks, &mut limit_order, false);
+	push_to_limit_order(bids, &mut limit_order, true);
+	// println!("limit order = {:#?}", limit_order);
+	limit_order
+}
 
-    println!("Connected to Deribit stream.");
-    println!("HTTP status code: {}", response.status());
-    println!("Response headers:");
-    for (ref header, ref header_value) in response.headers() {
-        println!("- {}: {:?}", header, header_value);
-    }
+fn push_to_limit_order(data: &Vec<serde_json::Value>, limit_order: &mut LimitOrder, is_bid: bool) {
+	let mut i = 0;
+	while i < data.len() {
+		// println!("BID = {:#?}", data[i]);
+		let order_data = OrderData {
+			order_type: data.get(i).unwrap().get(0).unwrap().as_str().unwrap().to_string(),
+			price: data.get(i).unwrap().get(1).unwrap().as_f64().unwrap(),
+			size: data.get(i).unwrap().get(2).unwrap().as_f64().unwrap()
+		};
+		// println!("{:#?}", order_data);
+		if is_bid {
+			limit_order.bids.push(order_data);
+		} else {
+			limit_order.asks.push(order_data);
+		}
+		i += 1;
+	}
+}
 
+fn update_order_book(limit_order_book: &mut LimitOrderBook, limit_order: &LimitOrder) {
+	update_orders(&mut limit_order_book.bids, &limit_order.bids, true);
+	update_orders(&mut limit_order_book.asks, &limit_order.asks, false);
+}
+
+fn update_orders(limit_order_book_orders: &mut Vec<OrderData>, limit_orders: &Vec<OrderData>, is_bid: bool) {
+	for order in limit_orders {
+		match order.order_type.as_str() {
+			"new" => {
+				let new_limit_order = OrderData {
+					order_type: order.order_type.clone(),
+					price: order.price,
+					size: order.size,
+				};
+				limit_order_book_orders.push(new_limit_order);
+				if is_bid {
+					limit_order_book_orders.sort_by(|a, b| b.price.partial_cmp(&a.price).unwrap() );
+				} else {
+					limit_order_book_orders.sort_by(|a, b| a.price.partial_cmp(&b.price).unwrap() );
+				}
+			}
+			"change" => {
+				let mut i = 0;
+				while i < limit_order_book_orders.len() {
+					if limit_order_book_orders[i].price == order.price {
+						limit_order_book_orders[i].size += order.size;
+					}
+					i += 1;
+				}
+			}
+			"delete" => {
+				let index = limit_order_book_orders.iter().position(|book_orders| book_orders.price == order.price).unwrap();
+				limit_order_book_orders.remove(index);
+			}
+			_ => {}
+		}
+		
+	}
+}
+
+fn get_limit_order_book(socket: &mut WebSocket<AutoStream>) -> LimitOrderBook {
 	socket.write_message(Message::text(r#"{
 		"method": "public/subscribe",
 		"params": {
@@ -66,35 +135,50 @@ fn main() {
 		"jsonrpc": "2.0",
 		"id": 3
 	  }"#)).expect("Error sending request");
-	let msg = get_msg(&mut socket);
-	println!("{}", msg);
-	let msg = get_msg(&mut socket);
+	let msg = get_msg(socket);
+	// println!("{}", msg);
+	let msg = get_msg(socket);
 	
 	let order_book_state_raw: serde_json::Value = serde_json::from_str(msg.as_str()).expect("Error parsing JSON");
-	println!("Book raw = {:#?}", order_book_state_raw);
+	// println!("Book raw = {:#?}", order_book_state_raw);
 	let asks = extract_data(&order_book_state_raw, "asks");
 	let bids = extract_data(&order_book_state_raw, "bids");
 
 	let mut limit_order_book = LimitOrderBook { bids: Vec::new(), asks: Vec::new() };
 	push_data_to_order_book(&mut limit_order_book, bids, true);
 	push_data_to_order_book(&mut limit_order_book, asks, false);
-	// println!("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\nLOB = {:#?}", limit_order_book);
+	limit_order_book
+}
+
+fn main() {
+    let deribit_url = format!(
+        "{}/public/subscribe",
+        DERIBIT_WS_API
+    );
+    let (mut socket, response) = connect(Url::parse(&deribit_url).unwrap()).expect("Can't connect to deribit websocket.");
+
+    println!("Connected to Deribit stream.");
+    println!("HTTP status code: {}", response.status());
+    println!("Response headers:");
+    for (ref header, ref header_value) in response.headers() {
+        println!("- {}: {:?}", header, header_value);
+    }
+
+	let mut limit_order_book = get_limit_order_book(&mut socket);
+	let mut limit_order: LimitOrder;
+	let mut i = 0;
     loop {
-		break;
-		// let msg = socket.read_message().expect("Error reading message");
-        // let msg = match msg {
-        //     tungstenite::Message::Text(s) => s,
-        //     _ => {
-        //         panic!("Error getting text");
-        //     }
-        // };
-		// println!("{}", msg);
-        // let parsed: models::DepthStreamWrapper = serde_json::from_str(&msg).expect("Can't parse");
-        // for i in 0..parsed.data.asks.len() {
-        //     println!(
-        //         "{}: {}. ask: {}, size: {}",
-        //         parsed.stream, i, parsed.data.asks[i].price, parsed.data.asks[i].size
-        //     );
-        // }
+		let msg = get_msg(&mut socket);
+		let mut order_state_raw: serde_json::Value = serde_json::from_str(msg.as_str()).expect("Error parsing JSON");
+		// let prev_limit_order = limit_order;
+		let limit_order: LimitOrder = populate_limit_order(&mut order_state_raw);
+		// if i > 0 && limit_order.prev_change_id != prev_limit_order.change_id {
+		// 	println!("Error: Packet lost reconnecting");
+		// 	break;
+		// }
+		update_order_book(&mut limit_order_book, &limit_order);
+		println!("Best bid = {:#?} Best Ask = {:#?}", limit_order_book.bids.get(0).unwrap().price, limit_order_book.asks.get(0).unwrap().price);
+		i += 1;
+		
     }
 }
--- src/models.rs
+++ src/models.rs
@@ -8,14 +8,14 @@ pub struct OrderData {
     pub size: f64,
 }
 
+
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct LimitOrder {
 	pub order_type: String,
-    pub timestamp: u32,
-    pub prev_change_id: u32,
-    pub change_id: u32,
-    pub instrument_name: String,
+    pub timestamp: u64,
+    pub prev_change_id: u64,
+    pub change_id: u64,
     pub bids: Vec<OrderData>,
     pub asks: Vec<OrderData>,
 }
